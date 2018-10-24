61cec42023f86fb725951958fecf3bef4f652800
hanleyjames <hanley.doth@outlook.com>
2018-10-24 10:46:47 -0700

    force fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
-import { NavComponent } from './nav/nav.component';
 
 const appRoutes: Routes = [
   {
@@ -11,16 +10,12 @@ const appRoutes: Routes = [
      component: MainComponent
    },
    {
-     path: 'Form',
+     path: 'form',
      component: FormComponent
    },
    {
-     path: 'About',
+     path: 'about',
      component: AboutComponent
-   },
-   {
-     path: 'Nav',
-     component: NavComponent
    }
 
 ];
